8b309503e9e67843df79efdffc537fa302694beb
marouaneaitelhaj <marwaneaitelhaj001@gmail.com>
2024-05-03 16:37:52 +0100

    Refactor import paths and types in Emploi.tsx component

--- app/pages/Emploi.tsx
+++ app/pages/Emploi.tsx
@@ -45,6 +45,9 @@ export default function Emploi() {
   return (
     <SafeAreaView className="h-screen bg-white">
       <Calendar
+      onMonthChange={(month) => {
+        setSelectedMonth(month.dateString.split("-")[0] + "-" + month.dateString.split("-")[1]);
+      }}
         markedDates={{
           [selectedMonth + "-" + selectedDay]: {
             selected: true,
