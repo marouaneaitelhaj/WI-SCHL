2187b25b0a0c4ca3ebcb22c293b31414f870880d
marouaneaitelhaj <marwaneaitelhaj001@gmail.com>
2024-05-10 09:38:34 +0100

    chore: Update npm dependencies to latest versions

--- state/Demandes/AttestationInscription/AttestationInscriptionActions.ts
+++ state/Demandes/AttestationInscription/AttestationInscriptionActions.ts
@@ -3,28 +3,33 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { TAttestationInscriptions } from "@state/types";
 import axios from "axios";
 
-export const getAttestationInscriptionValues = createAsyncThunk<
-  TAttestationInscriptions[],
-  void
->("attestationInscription/fetchValues", async () => {
-  const token = await AsyncStorage.getItem("token");
-  const response = await axios.get(
-    "http://ensemc.irma-prod.net/api/etudiant/attestation-inscription",
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }
-  );
-  return response.data.demAttestationInscriptions;
-});
+// crrete two async functions to fetch the data and to send the data
+// get => http://ensemc.irma-prod.net/api/etudiant/attestation-inscription
+// post => http://ensemc.irma-prod.net/api/etudiant/attestation-inscription/save-demande
+// with the token in the header
+// post request have no body
 
-export const demandeAttestation = createAsyncThunk(
-  "attestationInscription/saveDemande",
+export const getAttestationInscriptionValues = createAsyncThunk(
+  "attestationInscription/getAttestationInscriptionValues",
   async () => {
     const token = await AsyncStorage.getItem("token");
+    const response = await axios.get(
+      "http://ensemc.irma-prod.net/api/etudiant/attestation-inscription",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return response.data;
+  }
+);
 
-    const res = axios.post(
+export const demandeAttestation = createAsyncThunk(
+  "attestationInscription/demandeAttestation",
+  async () => {
+    const token = await AsyncStorage.getItem("token");
+    const response = await axios.post(
       "http://ensemc.irma-prod.net/api/etudiant/attestation-inscription/save-demande",
       {},
       {
@@ -33,7 +38,6 @@ export const demandeAttestation = createAsyncThunk(
         },
       }
     );
-
-    // return res.demAttestationInscriptions;
+    return response.data;
   }
 );
