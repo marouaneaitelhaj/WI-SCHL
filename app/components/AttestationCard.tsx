7d116060b1dab2cb3710890bd6adb55064d33621
marouaneaitelhaj <marwaneaitelhaj001@gmail.com>
2024-05-13 15:22:01 +0100

    Refactor import paths and types in AttestationCard.tsx and AttestationInscription.tsx components

--- app/components/AttestationCard.tsx
+++ app/components/AttestationCard.tsx
@@ -1,8 +1,9 @@
 import { Alert, Text, View } from "react-native";
-import { Inscriptions } from "../../state/types";
+import { TDemande } from "../../state/types";
 import { IconButton } from "react-native-paper";
 import * as FileSystem from "expo-file-system";
 import { shareAsync } from "expo-sharing";
+import { Linking } from 'react-native';
 import { useState } from "react";
 import Animated, {
   useAnimatedStyle,
@@ -14,7 +15,7 @@ import { cancelDemande } from "@state/Demandes/AttestationInscription/Attestatio
 import { Platform } from "react-native";
 
 export default function AttestationCard(props: {
-  data: Inscriptions;
+  data: TDemande;
 }) {
   const [expanded, setExpanded] = useState(false);
   const dispatch = useAppDispatch();
@@ -50,37 +51,6 @@ export default function AttestationCard(props: {
       color: "#4B0082",
     },
   };
-
-  async function saveFile(uri: string, filename: string, mimetype: string) {
-    if (Platform.OS === "android") {
-      console.log("helooooo");
-
-      const permissions =
-        await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
-
-      if (permissions.granted) {
-        const base64 = await FileSystem.readAsStringAsync(uri, {
-          encoding: FileSystem.EncodingType.Base64,
-        });
-
-        await FileSystem.StorageAccessFramework.createFileAsync(
-          permissions.directoryUri,
-          filename,
-          mimetype
-        )
-          .then(async (uri) => {
-            await FileSystem.writeAsStringAsync(uri, base64, {
-              encoding: FileSystem.EncodingType.Base64,
-            });
-          })
-          .catch((e) => console.log(e));
-      } else {
-        shareAsync(uri);
-      }
-    } else {
-      shareAsync(uri);
-    }
-  }
 
   const animatedStyles = useAnimatedStyle(() => {
     return {
@@ -186,22 +156,7 @@ export default function AttestationCard(props: {
                           text: "Télécharger",
                           style: "destructive",
                           onPress: () => {
-                            const uri = props.data.file;
-                            const filename = uri.substring(
-                              uri.lastIndexOf("/") + 1
-                            );
-                            const fileUri =
-                              FileSystem.documentDirectory + filename;
-
-                            FileSystem.downloadAsync(uri, fileUri, {})
-                              .then((result) => {
-                                saveFile(
-                                  result.uri,
-                                  filename,
-                                  result.headers["Content-Type"]
-                                );
-                              })
-                              .catch((e) => console.log(e));
+                            Linking.openURL(props.data.file)
                           },
                         },
                       ]
