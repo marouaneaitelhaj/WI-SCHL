320e625dc74a01a4a2d370ba13661e11023ac02a
marouaneaitelhaj <marwaneaitelhaj001@gmail.com>
2024-05-16 12:27:34 +0100

    Refactor import paths and update ReleveNote component to include session selection functionality

---
+++ app/pages/Demandes/ReleveNote/ReleveNoteForm.tsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import { Alert, Pressable } from "react-native";
+import {
+  createDemande,
+  cancelDemande,
+  getDemandes,
+} from "state/Demandes/ReleveNote/ReleveNoteActions";
+import { Checkbox, Text } from "react-native-paper";
+import { useAppDispatch } from "@state/store";
+
+export default function ReleveNoteForm() {
+  const [showModal, setShowModal] = useState(false);
+  const dispatch = useAppDispatch();
+
+  const [checkedNumber, setCheckedNumber] = useState(1);
+  return (
+    <>
+      <Checkbox.Item
+        onPress={() => setCheckedNumber(1)}
+        status={checkedNumber == 1 ? "checked" : "unchecked"}
+        label="1ère session"
+      />
+      <Checkbox.Item
+        onPress={() => setCheckedNumber(2)}
+        status={checkedNumber == 2 ? "checked" : "unchecked"}
+        label="2ème session"
+      />
+      <Checkbox.Item
+        onPress={() => setCheckedNumber(3)}
+        status={checkedNumber == 3 ? "checked" : "unchecked"}
+        label="Annuel"
+      />
+      <Pressable
+        className="bg-[#5156BE] my-2 p-2 rounded-md w-[100%] justify-center items-center"
+        onPress={() => {
+          setShowModal(false);
+          Alert.alert(
+            "Êtes-vous sûr de vouloir créer cette demande ?",
+            "Êtes-vous sûr de vouloir créer cette demande ?",
+            [
+              {
+                text: "Annuler",
+                style: "cancel",
+              },
+              {
+                text: "Confirmer",
+                style: "destructive",
+                onPress: () => {
+                  dispatch(createDemande(checkedNumber))
+                    .unwrap()
+                    .then((res) => {
+                      Alert.alert(
+                        "Demande envoyée",
+                        "Votre demande a été envoyée avec succès"
+                      );
+                    })
+                    .catch((err) => {
+                      Alert.alert(
+                        "Erreur",
+                        "Une erreur s'est produite lors de l'envoi de la demande"
+                      );
+                    });
+                },
+              },
+            ]
+          );
+        }}
+      >
+        <Text className="text-white">Confirmer</Text>
+      </Pressable>
+    </>
+  );
+}
---
+++ app/pages/Demandes/ReleveNote/index.tsx
@@ -0,0 +1,53 @@
+import AttestationCard from "app/components/AttestationCard";
+import { router } from "expo-router";
+import { useEffect, useState } from "react";
+import { ActivityIndicator } from "react-native";
+import { View, Text, Pressable, Alert, ScrollView } from "react-native";
+import { Checkbox, Title } from "react-native-paper";
+import { useSelector } from "react-redux";
+import {
+  createDemande,
+  cancelDemande,
+  getDemandes,
+} from "state/Demandes/ReleveNote/ReleveNoteActions";
+import { RootState, useAppDispatch } from "state/store";
+export default function ReleveNote() {
+  const dispatch = useAppDispatch();
+
+  const { data, status } = useSelector((state: RootState) => state.releveNote);
+  useEffect(() => {
+    dispatch(getDemandes());
+  }, []);
+  return (
+    <View className="space-y-5 flex items-center w-full">
+      <Title className="text-center font-bold uppercase">Relevé de notes</Title>
+      <ScrollView className="w-full h-[70%]">
+        <View className="flex w-full">
+          {status === "loading" && (
+            <ActivityIndicator size="large" color="#5156BE" />
+          )}
+          {status !== "loading" &&
+            data.map((item) => (
+              <AttestationCard
+                cancelDemande={cancelDemande}
+                key={item.num_dem}
+                data={item}
+              />
+            ))}
+        </View>
+      </ScrollView>
+      <View className="flex  w-full items-center">
+        <Pressable
+          className="flex  rounded-md w-[100%] justify-center items-center  p-5 bg-[#5156BE]"
+          onPress={() => {
+            router.push("/pages/Demandes/ReleveNote/ReleveNoteForm");
+          }}
+        >
+          <Text className="text-white text-center">
+            Déposer une nouvelle demande
+          </Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+}
