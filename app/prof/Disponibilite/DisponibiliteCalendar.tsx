d7c124205782924e24b3ba92fcb2a5908f8411a4
marouaneaitelhaj <marwaneaitelhaj001@gmail.com>
2024-05-30 14:16:55 +0100

    Refactor DisponibiliteCalendar component to improve file structure and fix bugs

--- app/prof/Disponibilite/DisponibiliteCalendar.tsx
+++ app/prof/Disponibilite/DisponibiliteCalendar.tsx
@@ -1,23 +1,24 @@
 import { View, Text, ScrollView, Pressable } from "react-native";
-import React, { useState } from "react";
+import React, { memo, useState } from "react";
 import { TSession, days, getSessionsByHour, hours } from "./data";
 import { TPeriode } from "@state/types";
 import { Icon } from "react-native-paper";
 
-export default function DisponibiliteCalendar(props: {
+function DisponibiliteCalendar(props: {
   title: string;
   periode: TPeriode;
 }) {
   const [expended, setExpended] = useState<boolean>(false);
+
   const findSession = (day: string, hour: string) => {
     return props.periode.disponibilites.some((dispo) => {
       if (dispo.day === day && dispo.hour === hour) {
-        console.log("found");
         return true;
       }
       return false;
     });
   };
+
   return (
     <Pressable className="bg-white flex" onPress={() => setExpended(!expended)}>
       <View className="flex flex-row justify-between w-full p-5 border-b border-[#E9E9EF]">
@@ -25,59 +26,73 @@ export default function DisponibiliteCalendar(props: {
         {!expended && <Icon source="chevron-down" size={30} color="#398AB9" />}
         {expended && <Icon source="chevron-up" size={30} color="#398AB9" />}
       </View>
-      {expended && (
-        <ScrollView horizontal={true}>
-          <ScrollView>
-            <View className="w-full flex items-end">
-              <View className="w-[88%] flex flex-row">
-                {days.map((day, index) => (
-                  <View
-                    key={index}
-                    className="flex justify-center h-12 w-20 border border-[#398AB9] items-center"
-                  >
-                    <Text className="text-center text-[#398AB9] font-bold">
-                      {day}
-                    </Text>
-                  </View>
-                ))}
-              </View>
-            </View>
-            {hours.map((hour, hourIndex) => (
-              <View key={hourIndex} className="flex flex-row">
-                <View className="w-[12%] flex items-center">
-                  <View className="flex justify-center h-12 w-20  border border-r-0 border-[#E9E9EF] items-center">
-                    <Text className="text-center">{hour}</Text>
-                  </View>
+      {/* {expended && ( */}
+      <ScrollView
+        horizontal={true}
+        style={{
+          height: expended ? "100%" : 0,
+        }}
+      >
+        <ScrollView>
+          <View className="w-full flex items-end">
+            <View className="w-[88%] flex flex-row">
+              {days.map((day, index) => (
+                <View
+                  key={index}
+                  className="flex justify-center h-12 w-20 border border-[#398AB9] items-center"
+                >
+                  <Text className="text-center text-[#398AB9] font-bold">
+                    {day}
+                  </Text>
                 </View>
-                <View className="flex flex-row flex-wrap">
-                  {getSessionsByHour(hour).map(
-                    (session: TSession, sessionIndex: number) => (
-                      <Pressable
-                        key={sessionIndex}
+              ))}
+            </View>
+          </View>
+          {hours.map((hour, hourIndex) => (
+            <View key={hourIndex} className="flex flex-row">
+              <View className="w-[12%] flex items-center">
+                <View className="flex justify-center h-12 w-20  border border-r-0 border-[#E9E9EF] items-center">
+                  <Text className="text-center">{hour}</Text>
+                </View>
+              </View>
+              <View className="flex flex-row flex-wrap">
+                {getSessionsByHour(hour).map(
+                  (session: TSession, sessionIndex: number) => (
+                    <Pressable
+                      key={sessionIndex}
+                      className={
+                        "flex justify-center h-12 w-20 border border-[#E9E9EF] items-center " +
+                        (findSession(session.day, session.hour)
+                          ? "bg-[#398AB9]"
+                          : "")
+                      }
+                    >
+                      <Text
                         className={
-                          "flex justify-center h-12 w-20 border border-[#E9E9EF] items-center " +
+                          "text-center " +
                           (findSession(session.day, session.hour)
-                            ? "bg-[#398AB9]"
+                            ? "text-white"
                             : "")
                         }
                       >
-                        <Text className={"text-center " + (findSession(session.day, session.hour) ? "text-white" : "")}>
-                          {session.day.charAt(0) +
-                            "/" +
-                            session.hour.charAt(0) +
-                            (session.hour.charAt(1) === ":"
-                              ? ""
-                              : session.hour.charAt(1))}
-                        </Text>
-                      </Pressable>
-                    )
-                  )}
-                </View>
+                        {session.day.charAt(0) +
+                          "/" +
+                          session.hour.charAt(0) +
+                          (session.hour.charAt(1) === ":"
+                            ? ""
+                            : session.hour.charAt(1))}
+                      </Text>
+                    </Pressable>
+                  )
+                )}
               </View>
-            ))}
-          </ScrollView>
+            </View>
+          ))}
         </ScrollView>
-      )}
+      </ScrollView>
+      {/* )} */}
     </Pressable>
   );
 }
+
+export default memo(DisponibiliteCalendar);
